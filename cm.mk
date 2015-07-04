82110a29333c71f1ae143d4b0767322eec4a24e4
Donald Hoskins <grommish@gmail.com>
2015-07-04 15:59:56 -0400

    Bring up d2refreshspr (S3 Triband L710T)
    Change-Id: Ifce25e729038ce42e754c6bd84321ef9a9a0c8ac

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/samsung/d2bst/full_d2bst.mk)
+$(call inherit-product, device/samsung/d2refreshspr/full_d2refreshspr.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
@@ -7,11 +7,11 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=d2bst \
-    TARGET_DEVICE=d2bst \
+    PRODUCT_NAME=d2refreshspr \
+    TARGET_DEVICE=d2refreshspr \
     BUILD_FINGERPRINT="samsung/d2lterefreshspr/d2lterefreshspr:4.4.2/KOT49H/L710TVPU0ANF4:user/release-keys" \
     PRIVATE_BUILD_DESC="d2lterefreshspr-user 4.4.2 KOT49H L710TVPU0ANF4 release-keys"
 
-PRODUCT_NAME := cm_d2bst
-PRODUCT_DEVICE := d2bst
+PRODUCT_NAME := cm_d2refreshspr
+PRODUCT_DEVICE := d2refreshspr
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo cm_d2bst-eng
+add_lunch_combo cm_d2refreshspr-eng
 
